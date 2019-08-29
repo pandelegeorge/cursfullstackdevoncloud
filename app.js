8eba6d8de0ddbac9b001c9f5217e2bd927629d8e
George Pandele <george.pandele.ext@man.apabucur.ro>
2019-08-29 11:17:26 +0300

    change port to 8080

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = 3000;
+const port = process.env.port || 8080;
 const bodyParser = require('body-parser');
 const itemRouter = require('./item.js');
 const bookRouter = require('./books.js');

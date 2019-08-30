ae556a17a4c2a90e25b4b7e430261541d8f43b7f
George Pandele <george.pandele.ext@man.apabucur.ro>
2019-08-30 13:00:08 +0300

    change database endpoint

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql')
 
 const con = mysql.createConnection({
-    host: "database-0-snapshots-apanova.ctjannfbohrq.eu-west-1.rds.amazonaws.com",
+    host: "database-0-snapshots-1.ctjannfbohrq.eu-west-1.rds.amazonaws.com",
     user: "admin",
     password: "apanova123#",
     database: "apanova"
